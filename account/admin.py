63a82c19f376839166f2bf615b6a099f58e2591c
asakeev11 <talgat.asakeev@gmail.com>
2022-09-01 19:04:16 +0600

    .

---
+++ account/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from account.models import CustomUser, Contact
+
+admin.site.register(CustomUser)
+admin.site.register(Contact)
---
+++ shop_api/urls.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework.routers import SimpleRouter
+from product.views import ProductViewSet
+from category.views import CategoryViewSet
+from cart.views import CartApiView
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+router = SimpleRouter()
+router.register('products', ProductViewSet)
+router.register('categories', CategoryViewSet)
+
+#TODO Category ViewSet -> Admin
+
+urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/v1/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/account/', include('account.urls')),
+    path('api/v1/cart/', CartApiView.as_view()),
+    path('api/v1/orders/', include('order.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
